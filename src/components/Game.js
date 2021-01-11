d3f725e182ddfc6de4735a45992e243387ec9226
Sagi Isaschar <sagi536@gmail.com>
2021-01-11 20:24:55 +0200

    6. toggling

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,6 +11,7 @@ const Game = ({ name, released, image, id }) => {
   //Load Details
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
+    console.log("s");
     document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
